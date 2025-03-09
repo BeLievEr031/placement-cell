61a24b7b00037f6dd8bc0f872d94fb2fa7fb6137
BeLieVer031 <sandyrajak031@gmail.com>
2025-03-09 12:16:36 +0530

    add cors origins

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,10 @@ const app = express();
 
 app.use(express.json({ limit: "1MB" }))
 app.use(express.urlencoded({ extended: true, limit: "1MB" }))
-
+app.use(cors({
+    credentials: true,
+    origin: ["http://localhost:5173", "http://localhost:5175"]
+}))
 
 app.use("/api/v1/placement/job", jobRouter)
 export default app;
